827e8b4baaa5040b1c39a6d869672b1a97cb0c33
kennyyhw <115073517+kennyyhw@users.noreply.github.com>
2023-06-02 21:27:08 +0000

    images, vijanden, border, etc.

--- script.js
+++ script.js
@@ -27,26 +27,37 @@ var vloerY = 650;
 
 var spelerX = 100; // x-positie van speler
 var spelerY = vloerY; // y-positie van speler
-var speler1 = 100;// x-positie van speler2
+var speler1 = 200;// x-positie van speler2
 var speler2 = vloerY;// y-positie van speler2
 
-var vijandX = 600; // x-positie van vijand
+var vijandX = 1150; // x-positie van vijand
 var vijandY = 500; // y-positie van vijand
+
+var vijandX2 = 800;
+var vijandY2 = 200;
 
 var spelerSpringt = false;
 var springSnelheid = 0;
-var springSnelheidStart = 8;
+var springSnelheidStart = 13;
 var zwaartekracht = 0.2;
 
 var spelerSpringt2 = false;
 var springSnelheid2 = 0;
-var springSnelheidStart2 = 8;
+var springSnelheidStart2 = 13;
 var zwaartekracht2 = 0.2;
+
+var speed= 5;
+var speed2 = 1.5;
+var speed3 = 2.5;
+var speed4 = 2;
+var speed5 = 3;
+var score = 0;
 
 var img;
 var img2;
 var img3;
 var img4;
+var img5;
 
 /* ********************************************* */
 /* functies die je gebruikt in je game           */
@@ -58,11 +69,11 @@ var img4;
 var beweegAlles = function() {
   // speler 1
   background('blue');
-  if (keyIsDown(LEFT_ARROW)) {
-    spelerX -= 5;
+   if (keyIsDown(LEFT_ARROW) && spelerX > 90) {
+    spelerX = spelerX - speed;
   }
-  if (keyIsDown(RIGHT_ARROW)) {
-    spelerX += 5;
+  if (keyIsDown(RIGHT_ARROW) && spelerX < 1250) {
+    spelerX = spelerX + speed;
   }
 
   if (spelerSpringt === false && keyIsDown(UP_ARROW)) {
@@ -83,16 +94,16 @@ var beweegAlles = function() {
   // speler 2
 
   //a
-  if (keyIsDown(65)) {
-    speler1 = speler1 -= 5;
+ 
+   if (keyIsDown(65) && speler1 > 90) {
+    speler1 = speler1 - speed;
   }
-  //d
-  if (keyIsDown(68)) {
-    speler1 = speler1 += 5;
+  if (keyIsDown(68) && speler1 < 1250) {
+    speler1 = speler1 + speed;
   }
   //w
   if (spelerSpringt2 === false && keyIsDown(87)) {
-    speler2 = speler2 - springSnelheid2;
+    speler2 = speler2 - springSnelheid;
     spelerSpringt2 = true;
     springSnelheid2 = springSnelheidStart2;
   }
@@ -108,9 +119,29 @@ var beweegAlles = function() {
 
 
   // vijand
+   vijandX += speed2 * speed3;
+  if (vijandX <= 75 || vijandX >= 1200) {
+    speed2 = -speed2;
+  }
+  if(score >= 10) {
+    speed3 = speed3 + 1
+  }
 
- 
+  if(score >= 11) {
+    speed3 = 4
+  }
 
+   vijandX2 += speed4 * speed5;
+  if (vijandX2 <= 75 || vijandX2 >= 1200) {
+    speed4 = -speed4;
+  }
+  if(score >= 10) {
+    speed5 = speed5 + 1
+  }
+
+  if(score >= 11) {
+    speed5 = 4
+  }
 }
 // kogel
 
@@ -138,6 +169,21 @@ var verwerkBotsing = function() {
     console.log("Botsing" + aantal);
   }
 
+  if (spelerX - vijandX2 < 50 &&
+    spelerX - vijandX2 > - 50 &&
+    spelerY - vijandY2 < 50 &&
+    spelerY - vijandY2 > - 50) {
+    aantal = aantal + 1
+    console.log("Botsing" + aantal);
+  }
+
+  if (speler1 - vijandX2 < 50 &&
+    speler1 - vijandX2 > -50 &&
+    speler2 - vijandY2 < 50 &&
+    speler2 - vijandY2 > -50) {
+    aantal = aantal + 1
+    console.log("Botsing" + aantal);
+  }
   // botsing kogel tegen vijand
 
   // update punten en health
@@ -149,10 +195,13 @@ var verwerkBotsing = function() {
  */
 var tekenAlles = function() {
   // achtergrond
-  background(143, 188, 240);
+  
+  background(img5);
  
  
- image(img4, vijandX - 100, vijandY - 100, 140, 140);
+ image(img4, vijandX - 100, vijandY - 100, 200, 140);
+
+  image(img4, vijandX2 - 100, vijandY2 - 100, 200, 140);
 
   // speler 2
 
@@ -174,19 +223,36 @@ var tekenAlles = function() {
  * anders return false
  */
 var checkGameOver = function() {
-  if (spelerX - vijandX < 50 &&
-    spelerX - vijandX > -50 &&
-    spelerY - vijandY < 50 &&
-    spelerY - vijandY > -50) {
+  if (spelerX - vijandX < 100 &&
+    spelerX - vijandX > -100 &&
+    spelerY - vijandY < 100 &&
+    spelerY - vijandY > -100) {
     aantal = aantal + 1
     console.log("Botsing" + aantal)
     return true;
   }
 
-  if (speler1 - vijandX < 50 &&
-    speler1 - vijandX > -50 &&
-    speler2 - vijandY < 50 &&
-    speler2 - vijandY > -50) {
+  if (speler1 - vijandX < 100 &&
+    speler1 - vijandX > -100 &&
+    speler2 - vijandY < 100 &&
+    speler2 - vijandY > -100) {
+    aantal = aantal + 1
+    console.log("Botsing" + aantal);
+    return true;
+  }
+     if (spelerX - vijandX2 < 100 &&
+    spelerX - vijandX2 > -100 &&
+    spelerY - vijandY2 < 100 &&
+    spelerY - vijandY2 > -100) {
+    aantal = aantal + 1
+    console.log("Botsing" + aantal)
+    return true;
+  }
+
+  if (speler1 - vijandX2 < 100 &&
+    speler1 - vijandX2 > -100 &&
+    speler2 - vijandY2 < 100 &&
+    speler2 - vijandY2 > -100) {
     aantal = aantal + 1
     console.log("Botsing" + aantal);
     return true;
@@ -199,10 +265,12 @@ var checkGameOver = function() {
 /* setup() en draw() functies / hoofdprogramma   */
 /* ********************************************* */
 function preload() {
-  img = loadImage('min.png');
-  img2 = loadImage('gru.png');
-  img3 = loadImage('blad.png');
-  img4 = loadImage('vec.png');
+  img = loadImage('afbeeldingen/min.png');
+  img2 = loadImage('afbeeldingen/gru.png');
+  img3 = loadImage('afbeeldingen/blad.png');
+  img4 = loadImage('afbeeldingen/ufo.gif');
+  img5 = loadImage('afbeeldingen/achtergrond.gif');
+ 
 
 }
 /**
@@ -214,9 +282,23 @@ function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
   createCanvas(1280, 720);
 
+}
 
-
-  background(143, 188, 240);
+var reset = function(){
+  spelerX = 100;
+  spelerY= vloerY;
+  speler1 = 200;
+  speler2 = vloerY;
+  vijandX = 1150;
+  vijandY = 500;
+  vijandX2 = 800;
+  vijandY2 = 200;
+  score = 0;
+  speed2 = 1.5;
+  speed3 = 2.5;
+  speed4 = 2;
+  speed5 = 3;
+  
 }
 
 /**
@@ -224,7 +306,6 @@ function setup() {
  * de code in deze functie wordt 50 keer per seconde
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
  */
-
 
 function draw() {
   if (spelStatus === SPELEN) {
@@ -243,12 +324,12 @@ function draw() {
     textSize(50);
     fill("white");
     image(img3, 350, 10, 650, 650);
-
+    reset();
     if (keyIsDown(32)) {//spatie
       spelerX = 100;
-      spelerY = 650;
-      speler1 = 150;
-      speler2 = 650;
+      spelerY = vloerY;
+      speler1 = 200;
+      speler2 = vloerY;
       spelStatus = SPELEN;
     }
 
